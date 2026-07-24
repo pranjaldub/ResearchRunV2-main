69c5d17a1ce16624dd4c4d924adc83d7e1ef9796
Pranjal Dubey <67390409+pranjaldub@users.noreply.github.com>
2026-07-24 18:12:19 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-export const API_BASE = `${BACKEND_URL}/api`;
+const API_BASE = `${BACKEND_URL.replace(/\/+$/, "")}/api`;
 
 const api = axios.create({ baseURL: API_BASE });
 
